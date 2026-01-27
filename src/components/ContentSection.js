af1919241c89348522aa9d0bcde428eb20391fa0
PalafoxM <palafox.marin31@gmail.com>
2026-01-27 14:22:52 -0600

    Observtorio

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -11,26 +11,26 @@ const ContentSection = () => {
           <img src={logo} width={150} height={150} title='COFOTUR' />
         </div>
       </div>
-      
+
       {/* Sección 2: Agregamos el botón "Entrar" aquí */}
       <div className="content-section2">
         <div className="content-box">
           <h3><p>Objetivo del Programa</p> </h3>
           Fortalecer el posicionamiento turístico de Guanajuato a nivel estatal, nacional e internacional mediante acciones de promoción alineadas con la estrategia de la SECTURI.
-          
+
         </div>
       </div>
-      
+
       <div className="content-section3">
         <div className="content-box3">
           <img src={logo2} width={180} height={75} title='COFOTUR' />
         </div>
       </div>
 
-      <a className="btn-entrar" href='http://localhost/cofotu' >
-            Entrar
-        </a>
-      
+      <a className="btn-entrar" href='https://secturnet.guanajuato.gob.mx/cofotu/' >
+        Entrar
+      </a>
+
     </>
   );
 };
